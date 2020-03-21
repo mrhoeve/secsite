05986f165a8742f8faee618421f28c6518756659
Marc Hoeve <mrhoeve@gmail.com>
2020-03-21 21:55:38 +0100

    Setupscript encrypts passwords now

--- setup/execute_sql.php
+++ setup/execute_sql.php
@@ -1,8 +1,9 @@
 <?php
 /*
- File intended to setup sql tables from sql file
- Run this only once since it drops the tables!!
- File based upon https://w3programmings.com/how-to-execute-sql-file-directly-in-php/
+ Dit PHP-script maakt de tabellen aan die gebruikt worden voor deze opdracht.
+ Let op! Het gegenereerde SQL-script verwijderd alle aanwezige tabellen!
+ SQL script gebaseerd op https://w3programmings.com/how-to-execute-sql-file-directly-in-php/
+ Het is wel hevig aangepast naar de specifieke omstandigheden van deze opdracht.
 */
 
 # Setup correct parameters
@@ -13,9 +14,27 @@ $mysql_password = "M@rt1n@1801";
 
 # MySQL with PDO_MYSQL
 $db = new PDO("mysql:host=$mysql_host;dbname=$mysql_database", $mysql_user, $mysql_password);
+# Lees het bestand in die gegenereerd is via MySQL Workbench
 $query = file_get_contents("create_db.sql");
+# Voer dit bestand in zijn geheel uit op de database
 $stmt = $db->prepare($query);
-if ($stmt->execute())
-    echo "Success";
-else
-    echo "Fail";
+if ($stmt->execute()) {
+    echo "Tabellen zijn succesvol aangemaakt\n\nDe aanwezige wachtwoorden worden direct versleuteld...\n";
+
+    $stmt = $db->prepare("select username, password from $mysql_database.user");
+    $stmt->execute();
+    $allUsers = $stmt->fetchAll();
+    $stmt = $db->prepare("update $mysql_database.user set password = :password where username = :username");
+    foreach ($allUsers as $user) {
+        echo "Wachtwoord voor gebruiker {$user['username']} wordt geüpdated\n";
+        $stmt->bindValue(":username", $user['username']);
+        $stmt->bindValue(":password", password_hash($user['password'], PASSWORD_BCRYPT));
+        $stmt->execute();
+    }
+
+    echo "\nOpdracht voltooid";
+
+} else {
+    echo "Tabellen zijn NIET succesvol aangemaakt...";
+}
+
